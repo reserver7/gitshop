e4ec9c48818061e64e56d19e54f3365fe942869b
Choigyujin <rbwls111822@gmail.com>
2021-03-12 02:44:30 +0900

    ggyu

--- top.js
+++ top.js
@@ -1,7 +1,3 @@
 let menu=["카테고리","로켓배송","로켓프레시","2020추석","로켓직구","골드박스","정기배송","이벤트/쿠폰","기획전","여행/티켓"];
 
-var html;
-menu.forEach(function(mkey,mnode){
-    document.getElementById("main_ul").innerHTML+="<li>"+mkey+"</li>";
-    console.log(mkey);
-});
+console.log(menu);
