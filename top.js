8ef5d9c3b0d1dda0b8d18868fadec1776bf1c5ce
Choigyujin <rbwls111822@gmail.com>
2021-03-14 16:32:30 +0900

    ggyu

--- top.js
+++ top.js
@@ -1,7 +1,11 @@
 $(function(){
     let $menu=["카테고리","로켓배송","로켓프레시","2020추석","로켓직구","골드박스","정기배송","이벤트/쿠폰","기획전","여행/티켓"];
 
-    $menu.each(function($key,$node){
-        $("#main_ul").append("<li>"+$key+"</li> ");
-    });
+    var $w=0;
+    var $ea=$menu.length;
+    while($w<$ea){
+        $("#main_ul").append("<li>"+$menu[$w]+"</li> ");
+        $w++;
+    }
 });
+
